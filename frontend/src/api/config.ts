498975e281d7f853bdb19b4e11f97787be3d89f8
Payd3r <62106049+Payd3r@users.noreply.github.com>
2025-04-15 19:07:12 +0200

    e

--- frontend/src/api/config.ts
+++ frontend/src/api/config.ts
@@ -1,6 +1,6 @@
 // const API_BASE_URL = 'http://192.168.0.23:3002';
-// const API_BASE_URL = 'http://101.58.39.17:3002';
-const API_BASE_URL = 'https://believe-totally-mines-houston.trycloudflare.com';
+const API_BASE_URL = 'http://101.58.39.17:3002';
+// const API_BASE_URL = 'https://believe-totally-mines-houston.trycloudflare.com';
 
 export const API_URLS = {
   base: API_BASE_URL,
@@ -13,8 +13,8 @@ export const API_URLS = {
 
 export const STATIC_URLS = {
   // images: 'http://192.168.0.23:3002',
-  // images: 'http://101.58.39.17:3002',
-  images: 'https://believe-totally-mines-houston.trycloudflare.com',
+  images: 'http://101.58.39.17:3002',
+  // images: 'https://believe-totally-mines-houston.trycloudflare.com',
 };
 
 export default API_URLS; 
